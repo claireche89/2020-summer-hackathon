5bf675b1c80e88a59a24e2b855f2467524e581c6
lmack335 <62677113+lmack335@users.noreply.github.com>
2020-08-27 20:08:13 -0400

    MyFirstApp Tutorial Completed

--- app/src/main/java/com/example/myfirstapp/SecondFragment.java
+++ app/src/main/java/com/example/myfirstapp/SecondFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.Random;
 
 public class SecondFragment extends Fragment {
 
@@ -22,6 +25,26 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // get the current count, get the string & format it with the count, set it as textview_header
+
+        Integer count = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
+        String countText = getString(R.string.random_heading, count);
+        TextView headerView = view.getRootView().findViewById(R.id.textview_header);
+        headerView.setText(countText);
+
+        //Get a random number between 0 and the count
+        Random random = new java.util.Random();
+        Integer randomNumber = 0;
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1);
+        }
+
+        // Convert that number into a string amd set it as tjhe text for textview_random
+        TextView randomView = view.getRootView().findViewById(R.id.textview_random);
+        randomView.setText(randomNumber.toString());
+
+
 
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
